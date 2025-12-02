a890ad4e792cb2a9761626b4986ff46c7ab5588a
techslb <techslb@outlook.com>
2025-12-02 14:50:38 -0600

    Create MadLibs.java

---
+++ MadLibs.java
@@ -0,0 +1,50 @@
+public class MadLibs {
+  /*
+  Your description here
+  */
+  	public static void main(String[] args){
+      /*
+      This program generates a mad libbed story.
+      Author: techslb
+      Date: 12/2/2025 
+      */
+      
+      // Protagonist defined here
+      String name1 = "Star Paladin Cross";
+
+      // Adjective block for the story
+      String adjective1 = "cool";
+      String adjective2 = "chalky";
+      String adjective3 = "silky";
+
+      // Verb block for the story
+      String verb1 = "fly";
+
+      // Noun block for the story
+      String noun1 = "dog";
+      String noun2 = "women";
+      String noun3 = "fire station";
+      String noun4 = "planet";
+      String noun5 = "radioactive waste";
+      String noun6 = "queen";
+
+      // Second character name here
+      String name2 = "Ricky Gervais";
+
+      // Number variable for story here
+      int number = 67;
+
+      // Place variable for story here
+      String place1 = "Mogadishu";
+
+      //The template for the story
+      String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+
+
+
+
+      // Time to read the story; Outputting the story here
+      System.out.println(story);
+      
+    }       
+}
